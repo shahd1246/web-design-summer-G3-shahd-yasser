99993357f3de0e278e8c5e6beb37f5892098b304
shahd1246 <shahdshnwy@gmail.com>
2026-07-21 14:18:50 +0300

    change-img

---
+++ New folder/script/script.js
@@ -0,0 +1,11 @@
+function changeImg(){
+    let img=document.querySelector('img')
+    if(img.src.includes('images/chemarro_about_a_design_of_a_pet_food_package_d4a19910-8b5f-4d58-b551-ed8984c3d276.png')){
+        img.src =
+        "images/chemarro_about_a_design_of_a_pet_food_package_d4a19910-8b5f-4d58-b551-ed8984c3d276 (1).png";
+    }else{
+        img.src =
+        "images/chemarro_about_a_design_of_a_pet_food_package_d4a19910-8b5f-4d58-b551-ed8984c3d276.png"
+    }
+
+}
--- bank/bank-task/script/script.js
+++ bank/bank-task/script/script.js
@@ -16,18 +16,19 @@ let users = [
   },
 ];
 
-console.log(users)
+localStorage.setItem("users", JSON.stringify(users));
+let newUsers = JSON.parse(localStorage.getItem("users"));
 
 function addUsers() {
   let UserId = Number(prompt("enter user id"));
-  let existedId = users.some((item) => item.id == UserId);
+  let existedId =newUsers.some((item) => item.id == UserId);
   if(existedId){
     alert('Existed id')
     return
   }
     let UserName = prompt("enter user name");
     let Userbalance = Number(prompt("enter user balance"));
-    users.push({
+    newUsers.push({
       id: UserId,
       name: UserName,
       balance: Userbalance,
@@ -36,37 +37,40 @@ function addUsers() {
 addUsers();
 addUsers();
 addUsers();
-
-console.log(users);
+localStorage.setItem("users", JSON.stringify(newUsers));
 
 function editUserBalance() {
   let editById = prompt("enter the id which you want edit its balance");
   let editedBalance =Number(prompt("enter the new balance"));
 
-  let editUser = users.findIndex((item) => item.id == editById);
-   users[editUser].balance = editedBalance;
+  let editUser = newUsers.findIndex((item) => item.id == editById);
+   newUsers[editUser].balance = editedBalance;
 }
 editUserBalance();
+localStorage.setItem("users", JSON.stringify(newUsers));
 
-console.log(users);
 
 function transferBalance(){
     let fromId=Number(prompt('Enter the id you want transfer from'))
     let toId=Number(prompt('Enter the id you want transfer to'))
     let newBalance=Number(prompt('Enter the amount you want transfer'))
-    let fromIndx=users.findIndex(item=> item.id===fromId)
-    let toIndx=users.findIndex(item=> item.id===toId)
-    users[fromIndx].balance-=newBalance
-    users[toIndx].balance+=newBalance
+    let fromIndx=newUsers.findIndex(item=> item.id===fromId)
+    let toIndx = newUsers.findIndex((item) => item.id === toId);
+    newUsers[fromIndx].balance -= newBalance;
+    newUsers[toIndx].balance += newBalance;
 }
 transferBalance()
-console.log(users)
+localStorage.setItem("users", JSON.stringify(newUsers));
+
 
 function deleteUser(){
     let deletedUser=Number(prompt('Inter users id who you want remove'))
-    let deleteIndex=users.findIndex(item=> item.id===deletedUser)
-    users.splice(deleteIndex,1);
+    let deleteIndex = newUsers.findIndex((item) => item.id === deletedUser);
+    newUsers.splice(deleteIndex, 1);
     
 }
 deleteUser()
-console.log(users);
+localStorage.setItem("users", JSON.stringify(newUsers));
+
+
+console.log(newUsers);
